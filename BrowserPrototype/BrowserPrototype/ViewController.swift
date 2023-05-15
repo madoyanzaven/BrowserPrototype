cd373aae65c33cfed59eeb944832b4c8a4ed846a
Zaven Madoyan <madoyanzaven@gmail.com>
2023-05-15 10:54:51 +0400

    create app

---
+++ BrowserPrototype/BrowserPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrowserPrototype
+//
+//  Created by Zaven Madoyan on 15.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
